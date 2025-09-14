ae567e8cd60c30c9dfe7bb5a725bf294670e61f9
d <d'@DESKTOP-6BHOUR4>
2025-09-14 20:38:04 -0300

    Method POST

--- Program.cs
+++ Program.cs
@@ -6,5 +6,29 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //list
+List<Tasks> tasks = new List<Tasks>();
+int nextId = 1;
+
+app.MapGet("/tasks", () => tasks);
+
+app.MapPost("/tasks", (Tasks task) =>
+{
+    if (string.IsNullOrEmpty(task.Name))
+    {
+        return Results.BadRequest();
+    }
+
+    if (task.Name.Length < 3)
+    {
+        return Results.BadRequest("name is too short!");
+    }
+
+    task.Id = nextId;
+    nextId++;
+
+    tasks.Add(task);
+
+    return Results.Ok($"{task} successfully added");
+});
 
 app.Run();
